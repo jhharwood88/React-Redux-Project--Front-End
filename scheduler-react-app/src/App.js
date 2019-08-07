2c0a9fae032ee36f8f2de0b5a671bc245c2062d6
Jon <github email address>
2019-08-07 16:26:28 -0400

    added routes for shift container, imports

--- scheduler-react-app/src/App.js
+++ scheduler-react-app/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/Home'
+import ShiftsContainer from './containers/ShiftsContainer'
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path "/" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route path="/shifts" component={ShiftsContainer} />
       </div>
     </Router>
   );
